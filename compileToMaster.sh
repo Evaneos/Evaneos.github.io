dd2a28e8183c0db13b29f8c1dfefaacd67e7be5b
Christophe Hurpeau <christophe@hurpeau.com>
2014-04-06 18:24:49 +0200

    compileToMaster script

---
+++ compileToMaster.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Script manuel, en attendant la version avec drone.io
+
+if [[ -n `git status --porcelain | tr -d '\n'` ]]; then
+	echo "You need to commit before compiling to master"
+	exit 1	
+fi
+
+git checkout sources
+jekyll build || exit 1
+git checkout master
+git ls-files | xargs rm
+find . -depth -type d -empty -exec rmdir {} \;
+mv _site/* .
+rmdir _site
+
+git add -A
+git commit -m "auto commit"
+git push origin master
+git checkout sources
